dde4e60c35f6333a85b231d6a67929a098be41cb
leticiapmarchi <leticiapmarchi@gmail.com>
2022-06-05 15:46:01 -0300

    cores

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,40 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Atenção',
+      message: 'Deja favoritar o filme?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Sim',
+          id: 'confirm-button',
+          handler: () => {
+            this.apresentarToast();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Vídeo adicionado ao favoritos!',
+      duration: 2000,
+      color: 'success'
+    });
+    toast.present();
+  }
 
 }
